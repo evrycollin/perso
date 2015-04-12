8860f2f959a1f06098b32657e0fd05d3dced9c00
evrycollin <evry.collin@gmail.com>
2015-04-12 14:47:36 +0200

    v

--- jboss-vertica-hibernate4/src/main/java/org/jboss/as/quickstart/hibernate4/rest/MemberResource.java
+++ jboss-vertica-hibernate4/src/main/java/org/jboss/as/quickstart/hibernate4/rest/MemberResource.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.as.quickstart.hibernate4.web;
+package org.jboss.as.quickstart.hibernate4.rest;
 
 import java.util.List;
 
